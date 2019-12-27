2d3858855e7ecc938ba1868fdbe3e5be74392a8a
Rowland1318 <christina.a.rowland@gmail.com>
2019-12-27 11:15:20 -0500

    complete objective1lab3

---
+++ Labs/Objective1Lab3.java
@@ -0,0 +1,7 @@
+public class Objective1Lab3 {
+  public static void main(String[] args) {
+    System.out.println("Name: Christina Rowland");
+    System.out.println("Age: 28");
+    System.out.println("State: New Jersey");
+  }
+}
