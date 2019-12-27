c3ab2b6b7bd73cab97ae3e43351a001795c072ae
Rowland1318 <christina.a.rowland@gmail.com>
2019-12-27 12:00:56 -0500

    complete Objective2Lab2

---
+++ Labs/Objective2Lab2.java
@@ -0,0 +1,11 @@
+public class Objective2Lab2 {
+  public static void main(String[] args) {
+    String name = "Christina Rowland";
+    int age = 28;
+    String state = "New Jersey";
+
+    System.out.print("Name: " + name);
+    System.out.print(" Age: " + age);
+    System.out.println(" State: " + state);
+  }
+}
