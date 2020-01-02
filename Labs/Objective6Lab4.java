a522c0cb53904baf28328395855bf1e749c59c4e
Rowland1318 <christina.a.rowland@gmail.com>
2020-01-02 14:52:15 -0500

    complete Objective6Lab4

---
+++ Labs/Objective6Lab4.java
@@ -0,0 +1,13 @@
+public class Objective6Lab4 {
+  public static void main(String[] args) {
+    int total, counter;
+    total = 0;
+    counter = 1;
+
+    while(counter <= 20) {
+        total = total + counter;
+        counter++;
+      }
+    System.out.println(total);
+  }
+}
