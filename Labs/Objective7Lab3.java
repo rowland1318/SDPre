976c72560fd8c613a237c611497c52879b8efd10
Rowland1318 <christina.a.rowland@gmail.com>
2020-01-05 16:33:21 -0600

    complete Objective7Lab3

---
+++ Labs/Objective7Lab3.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class Objective7Lab3 {
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    int selection;
+
+    while(true){
+      printMenu();
+      selection = scanner.nextInt();
+
+      if (selection == 1) {
+        System.out.println("Hello Human");
+      }
+
+      if (selection == 2) {
+        System.out.println("Apples, Bananas, Coconuts");
+      }
+
+      if (selection == 3) {
+        System.out.println("Goodbye");
+        scanner.close();
+        break;
+      }
+
+    }
+
+  }
+
+  public static void printMenu() {
+    System.out.println("____Menu____");
+    System.out.println("1: Say Hello");
+    System.out.println("2: List My favorite foods");
+    System.out.println("3: Exit");
+    System.out.println();
+  }
+}
